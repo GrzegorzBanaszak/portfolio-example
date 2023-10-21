0539e8d2854a25252538e6de3e4395cc7a58dcd7
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-10-21 19:03:57 +0200

    Testimonials styles complite

--- components/TestimonialSlider.jsx
+++ components/TestimonialSlider.jsx
@@ -1,3 +1,19 @@
+import nextConfig from "../next.config";
+// icons
+import { FaQuoteLeft } from "react-icons/fa";
+
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//import swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+//import required modules
+import { Navigation, Pagination } from "swiper";
+import Image from "next/image";
+
 // testimonial data
 const testimonialData = [
   {
@@ -24,7 +40,54 @@ const testimonialData = [
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className="h-[400px]"
+    >
+      {testimonialData.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="flex flex-col md:flex-row gap-x-8 h-full px-16">
+              {/* avatar , name , position */}
+              <div className="w-full max-x-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div className="flex flex-col justify-center text-center">
+                  {/* avatar */}
+                  <div className="mb-2 mx-auto">
+                    <Image
+                      src={nextConfig.basePath + person.image}
+                      width={100}
+                      height={100}
+                      alt=""
+                    />
+                  </div>
+                  {/* name */}
+                  <div className="text-lg">{person.name}</div>
+                  {/* position */}
+                  <div className="text-[12px] uppercase font-extralight tracking-widest">
+                    {person.position}
+                  </div>
+                </div>
+              </div>
+              {/* quote & message */}
+              <div className=" flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+                <div className="mb-4">
+                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
+                </div>
+                <div className="xl:text-lg text-center md:text-left">
+                  {person.message}
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
